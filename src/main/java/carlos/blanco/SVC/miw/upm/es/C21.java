d08a95b523b19ab3b8f4f22ef01d6d00e8216022
maste <maste@LAPTOP-9Q7VHT04>
2017-09-26 20:27:35 +0200

    Create C21

---
+++ src/main/java/carlos/blanco/SVC/miw/upm/es/C21.java
@@ -0,0 +1,13 @@
+package carlos.blanco.SVC.miw.upm.es;
+
+public class C21 {
+    
+    public String m1() {
+        return "m1";
+    }
+    
+    public String m2() {
+        return "m2";
+    }
+    
+}
