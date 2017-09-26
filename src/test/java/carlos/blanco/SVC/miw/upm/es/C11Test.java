0e9049fb3894c9c80ee95f33e789e2940f1e61c2
maste <maste@LAPTOP-9Q7VHT04>
2017-09-26 20:08:41 +0200

    Create C11 Create C11Test Test Unitarios

---
+++ src/test/java/carlos/blanco/SVC/miw/upm/es/C11Test.java
@@ -0,0 +1,25 @@
+package carlos.blanco.SVC.miw.upm.es;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public abstract class C11Test {
+    
+    private C11 c11;
+
+    @Before
+    public void before() {
+        c11 = new C11();
+    }
+    
+    @Test
+    public void testM1() {
+        assertEquals("m1", c11.m1());
+    }
+    @Test
+    public void testM2() {
+        assertEquals("", c11.m2());
+    }
+}
