a3f313a03d3965e435d17636a67a8f80a1ad5cc4
maste <maste@LAPTOP-9Q7VHT04>
2017-09-26 21:13:31 +0200

    Create C32Test

---
+++ src/test/java/carlos/blanco/SVC/miw/upm/es/C32Test.java
@@ -0,0 +1,22 @@
+package carlos.blanco.SVC.miw.upm.es;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C32Test {
+
+    private C32 c32;
+
+    @Before
+    public void before() {
+        c32 = new C32();
+    }
+
+    @Test
+    public void testMA() {
+        assertEquals("mA", c32.mA());
+    }
+
+}
