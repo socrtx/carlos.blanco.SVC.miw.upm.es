a6a7ee7cb03247e70a565b5efc961c0c118513f0
maste <maste@LAPTOP-9Q7VHT04>
2017-09-26 21:29:01 +0200

    Prepare release 2

--- src/main/java/carlos/blanco/SVC/miw/upm/es/C11.java
+++ src/main/java/carlos/blanco/SVC/miw/upm/es/C11.java
@@ -2,13 +2,16 @@ package carlos.blanco.SVC.miw.upm.es;
 
 public class C11 {
     
+    
+    public String m1() {
+        return "m1";
+    } 
+    
     public String m2() {
         return "m2";
     }
     
-    public String m1() {
-        return "m1";
-    }
+
     
 
 }
