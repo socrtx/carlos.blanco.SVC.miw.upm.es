4f15aafa5f53de0df9014eaca923e75a1379960c
maste <maste@LAPTOP-9Q7VHT04>
2017-09-26 20:30:44 +0200

    Create C21Test

---
+++ src/test/java/carlos/blanco/SVC/miw/upm/es/C21Test.java
@@ -0,0 +1,26 @@
+package carlos.blanco.SVC.miw.upm.es;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class C21Test {
+    private C21 c21;
+
+    @Before
+    public void before() {
+        c21 = new C21();
+    }
+    
+    @Test
+    public void testM1() {
+        assertEquals("", c21.m1());
+    }
+    
+    @Test
+    public void testM2() {
+        assertEquals("", c21.m2());
+    }
+
+}
